432b6aebdd95ad89e253afd0ac326720ef11e5d6
Vipul <Vipul@DESKTOP-J10MEMJ>
2026-04-19 14:43:07 +0530

    First Commit

---
+++ maven-project/src/test/java/demo/SampleTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void Case1() {
+		System.out.println("Test Case 1");
+	}
+	@Test
+	public void Case2() {
+		System.out.println("Test Case 1");
+	}
+	@Test
+	public void Case3() {
+		System.out.println("Test Case 1");
+	}
+
+}
---
+++ maven-project/src/test/java/sample/DemoTest.java
@@ -0,0 +1,19 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void Case11() {
+		System.out.println("Test Case 11");
+	}
+	@Test
+	public void Case12() {
+		System.out.println("Test Case 11");
+	}
+	@Test
+	public void Case13() {
+		System.out.println("Test Case 11");
+	}
+
+}
